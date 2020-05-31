d01dff1fc533805fc1fd1faaf6f604e3bfcace64
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2020-05-31 18:10:24 +0530

    Added CSS

--- assets/js/tapanshu.js
+++ assets/js/tapanshu.js
@@ -14,9 +14,9 @@ for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", () => {
             swipper.style.transition = "transform 0.5s ease-in-out";
 
-            swipper.style.transform = 'translate3d(' + (-size * i  ) + 'px,' + 0 + 'px,' + 0 + 'px)';
+            swipper.style.transform = 'translate3d(' + (-size * i) + 'px,' + 0 + 'px,' + 0 + 'px)';
             image[i].style.transform = 'rotateX(' + 0 + 'deg)' + 'rotateY(' + 0 + 'deg)';
-            
+
 
 
 
@@ -30,3 +30,11 @@ for (let i = 0; i < button.length; i++) {
     );
 
 }
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion() {
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
