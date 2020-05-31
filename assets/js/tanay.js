8fd31827851e2dc17a47c603510250b90ea8a94e
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2020-05-31 18:05:40 +0530

    QnA is added ✔

--- assets/js/tanay.js
+++ assets/js/tanay.js
@@ -22,3 +22,12 @@ $(document).ready(function() {
         } // End if
     });
 });
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion() {
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
