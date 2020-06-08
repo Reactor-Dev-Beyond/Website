a1fbcdacdb2c15026515f8687b8eb2a049731ba4
Frosty <12583766+iFrostyFlame@users.noreply.github.com>
2020-06-08 14:51:18 +0530

    Modified Code

--- assets/js/tapanshu.js
+++ assets/js/tapanshu.js
@@ -1,7 +1,6 @@
 const button = document.querySelectorAll(".sch-btn");
 let i = 0;
-const swipper = document.querySelector(".swiper-wrapper");
-const page = document.getElementsByClassName("swiper-pagination-bullet");
+const swipper = document.querySelector(".swiper-wrapper")
 const image = document.querySelectorAll(".swiper-slide");
 //let's do this!!
 const size = image[i].clientWidth;
@@ -10,7 +9,7 @@ console.log(image.length);
 
 let move = 0;
 
-/*for (let i = 0; i < button.length; i++) {
+for (let i = 0; i < button.length; i++) {
 
     button[i].addEventListener("click", () => {
             swipper.style.transition = "transform 0.5s ease-in-out";
@@ -29,27 +28,5 @@ let move = 0;
 
 
     );
-
-} */
-for (let i = 0; i < page.length; i++) {
-
-    page[i].addEventListener("click", () => {
-        pageactive = document.getElementsByClassName("swiper-pagination-bullet-active");
-        pageactive[0].className = pageactive[0].className.replace("swiper-pagination-bullet-active", "");
-        page[i].className += " swiper-pagination-bullet-active";
-
-        swipper.style.transition = "transform 0.5s ease-in-out";
-
-        swipper.style.transform = 'translate3d(' + (-size * i) + 'px,' + 0 + 'px,' + 0 + 'px)';
-        image[i].style.transform = 'rotateX(' + 0 + 'deg)' + 'rotateY(' + 0 + 'deg)';
-
-
-
-
-
-
-        console.log("tapanshu bhai");
-    });
-
 
 }
