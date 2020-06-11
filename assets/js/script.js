e8cbc6d37cc0c388bc6783ed36444b425b0e979d
Frosty <12583766+iFrostyFlame@users.noreply.github.com>
2020-06-11 14:13:26 +0530

    fixed pagination and faq

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,6 @@
 const items = document.querySelectorAll(".accordion a");
 let para = document.querySelectorAll('.inside');
 let link = document.querySelectorAll('.accordion a');
-const button = document.querySelectorAll(".sch-btn");
-let i = 0;
-const swipper = document.querySelector(".swiper-wrapper")
-const image = document.querySelectorAll(".swiper-slide");
-const size = image[i].clientWidth;
 let move = 0;
 
 $(document).ready(function() {
@@ -36,7 +31,6 @@ $(document).ready(function() {
 for (let i = 0; i < para.length; i++) {
 
     items[i].addEventListener("click", () => {
-
         if (para[i].style.display === "none") {
 
             para[i].style.display = "block";
@@ -48,21 +42,11 @@ for (let i = 0; i < para.length; i++) {
     });
 }
 
-
-
 function toggleAccordion() {
     this.classList.toggle('active');
     this.nextElementSibling.classList.toggle('active');
 }
 
-for (let i = 0; i < button.length; i++) {
-
-    button[i].addEventListener("click", () => {
-        swipper.style.transition = "transform 0.5s ease-in-out";
-        swipper.style.transform = 'translate3d(' + (-size * i) + 'px,' + 0 + 'px,' + 0 + 'px)';
-        image[i].style.transform = 'rotateX(' + 0 + 'deg)' + 'rotateY(' + 0 + 'deg)';
-    });
-}
 
 
 (function($) {
@@ -122,6 +106,7 @@ var swiper = new Swiper(".swiper-container", {
     },
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
     },
 });
 
